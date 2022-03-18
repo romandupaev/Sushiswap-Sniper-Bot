5ac38fcf4eff45ebb1a42609363776e86c069d55
Tyler <101844570+r1ghtl3ft@users.noreply.github.com>
2022-03-18 22:38:25 +0200

    Add files via upload

---
+++ configfile.py
@@ -0,0 +1,50 @@
+import sys
+
+
+##Newly added variables v2.6
+buytimes='1'
+enablestoploss='0'
+enabletokenbuy='0'
+ttokenbuy='0'
+sstoploss='0'
+##
+
+token_address=''
+decimals='18'
+bnb_amount='0.0001'
+speedms='0'
+sec_wait='1'
+max_slippage='10'
+gwei='5'
+infura_url='https://bsc-dataseed3.defibit.io/'
+addedurl=''
+enableprofitsell='1'
+pprofitsell='10'
+
+enable_waiting='1'
+liq_before='1'
+approvetoken='1'
+
+enable_pricebuying='1'
+enable_priceselling='1'
+enablesellslippage='1'
+onlysell='0'
+
+
+gasusage='500000'
+sellslippage='40'
+pricebuy='2'
+pricesell='0.5'
+selltimesenable='0'
+selltimes='1'
+version='2'
+
+
+retrywhenerror='0'
+amount_threads='1'
+getclipboard='1'
+my_address = ''
+my_pk = ''
+
+
+sys.path.append(".")
